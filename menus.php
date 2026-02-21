b7912913654ad6a3f9d71c9af1f8f0ac1555a1b5
Diluc Ragvindr <marlinsaf@gmail.com>
2026-02-21 16:29:34 +0100

    changements au ficghier menusPost.php

--- menus.php
+++ menus.php
@@ -13,7 +13,31 @@ include('header.html');
 <body>
     <h1>VITE ET GOURMAND</h1>
     
-    
+<form action="menusPost.php" method="POST">
+    <div class=formDiv1>
+        <label for="theme">Thème : </label>
+        <input type="text" name="theme"  /> <br><br>
+
+        <label for="maxPrice">Prix maximum : </label>
+        <input type="integer" name="maxPrice"  /> <br><br>
+
+        <label for="minPrice">Prix minimum : </label>
+        <input type="number" name="minPrice" /> <br><br>
+
+        <label for="diet">Régime alimentaire : </label>
+        <select name="diet" id="diet">
+            <option value="vegetarien">Veretarien</option>
+            <option value="Vegan">Vegan</option>
+            <option value="Classic">Clasique</option>
+        </select><br><br>
+
+        <label for="minQtity">Nombre de personnes minimum :</label>
+        <input type="number" name="minQtity"/> <br><br>
+    </div>
+<br><br>
+    <button class="button" type="submit">Valider</button><br><br>
+</form>
+
 
 
 
--- menusPost.php
+++ menusPost.php
@@ -1,4 +1,19 @@
 <?php
+include('header.html');
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Vite Et Gourmand - Menus</title>
+</head>
+<body>
+    <h1>VITE ET GOURMAND</h1>
+    
+<?php
 
 $dsn = 'mysql:host=localhost;dbname=vite_et_gourmand';
 $username = 'user_gourmand';
@@ -8,17 +23,19 @@ $password = '';
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $menus=[];
     $id=1;
+    $diet = $_POST['diet'];
 
-    $query = "SELECT * FROM menus" ;
+
+    $query = "SELECT * FROM menus where diet = :diet" ;
 
     $stmt = $pdo->prepare($query);
-    $stmt->bindParam(':id', $id);
+    $stmt->bindParam(':diet', $diet);
     $stmt->execute();
 
     if($stmt->rowCount() != 0){
             $menus = $stmt->fetch(PDO::FETCH_ASSOC);
         foreach($menus as $menu){
-        echo $menu;
+            echo($menu);
         }
     }
     else{
@@ -30,4 +47,10 @@ catch (PDOException $e){
     echo "Erreur de connexion à la base de données : ". $e->getMessage();
 }
 
+?>
+
+</body>
+</html>
+<?php
+include('footer.html');
 ?>
