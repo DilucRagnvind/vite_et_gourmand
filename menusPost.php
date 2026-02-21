475cb99d7670bf3c329fde06564f268d0a343f4f
Diluc Ragvindr <marlinsaf@gmail.com>
2026-02-21 15:52:06 +0100

    changements au ficghier menusPost.php

--- menusPost.php
+++ menusPost.php
@@ -3,26 +3,26 @@
 $dsn = 'mysql:host=localhost;dbname=vite_et_gourmand';
 $username = 'user_gourmand';
 $password = '';
-$menus=[];
  try{
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-foreach($menus as $menu){
+    $menus=[];
     $id=1;
-    $query = "SELECT * FROM menus where id = :id " ;
+
+    $query = "SELECT * FROM menus" ;
 
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(':id', $id);
     $stmt->execute();
 
-    if($stmt->rowCount() == 1){
-        $menus = $stmt->fetch(PDO::FETCH_ASSOC);
-        echo $query;
-        $id++;
+    if($stmt->rowCount() != 0){
+            $menus = $stmt->fetch(PDO::FETCH_ASSOC);
+        foreach($menus as $menu){
+        echo $menu;
+        }
     }
-
-    else{echo "Connectez vous pour commander!"; 
-}
+    else{
+        echo "Connectez vous pour commander!"; 
 
     } 
 }
